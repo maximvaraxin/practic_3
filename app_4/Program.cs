429ff94aa8bba328e18d5a873f92c380c35ed791
Максим Вараксин <maks-02@yandex.ru>
2022-07-01 21:27:36 +0300

    add app_1..4(no finaly)

---
+++ app_4/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+ *  5 -> 1, 4, 9, 16, 25.
+ *  2 -> 1,4
+ */
+
+namespace App_4
+{
+    class Programm
+    {
+        static void Main ( string[] args )
+        {   
+            Console.Write( $" Введите число: " );
+            double num = Convert.ToInt32( Console.ReadLine() );
+
+            GetSqrt( num );
+        }
+
+        static void GetSqrt( double number )
+        {
+            double counter = 1;
+
+            Console.Write( $" Результат: ");
+           
+            while( number >= counter )
+            {
+                Console.Write( $"{ counter * counter } ");
+                counter++;
+            }
+        }
+    }
+}   
